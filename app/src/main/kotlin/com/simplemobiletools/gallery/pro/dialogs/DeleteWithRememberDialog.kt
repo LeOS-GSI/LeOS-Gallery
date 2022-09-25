8a598d871675b6349d7ffa65e7511ec67c9e6373
hojat <hojat72elect@gmail.com>
2022-09-25 01:54:13 -0400

    Ticket#5 Migrated DeleteWithRememberDialog from Synthetic to ViewBinding

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/dialogs/DeleteWithRememberDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/dialogs/DeleteWithRememberDialog.kt
@@ -1,26 +1,27 @@
 package com.simplemobiletools.gallery.pro.dialogs
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import androidx.appcompat.app.AlertDialog
 import com.simplemobiletools.commons.extensions.getAlertDialogBuilder
 import com.simplemobiletools.commons.extensions.setupDialogStuff
 import com.simplemobiletools.gallery.pro.R
-import kotlinx.android.synthetic.main.dialog_delete_with_remember.view.*
+import com.simplemobiletools.gallery.pro.databinding.DialogDeleteWithRememberBinding
 
 class DeleteWithRememberDialog(val activity: Activity, message: String, val callback: (remember: Boolean) -> Unit) {
+
+    // we create the binding by referencing the owner Activity
+    var binding = DialogDeleteWithRememberBinding.inflate(activity.layoutInflater)
+
     private var dialog: AlertDialog? = null
 
-    @SuppressLint("InflateParams")
-    val view = activity.layoutInflater.inflate(R.layout.dialog_delete_with_remember, null)!!
 
     init {
-        view.delete_remember_title.text = message
+        binding.deleteRememberTitle.text = message
         activity.getAlertDialogBuilder()
             .setPositiveButton(R.string.yes) { _, _ -> dialogConfirmed() }
             .setNegativeButton(R.string.no, null)
             .apply {
-                activity.setupDialogStuff(view, this) { alertDialog ->
+                activity.setupDialogStuff(binding.root, this) { alertDialog ->
                     dialog = alertDialog
                 }
             }
@@ -28,6 +29,6 @@ class DeleteWithRememberDialog(val activity: Activity, message: String, val call
 
     private fun dialogConfirmed() {
         dialog?.dismiss()
-        callback(view.delete_remember_checkbox.isChecked)
+        callback(binding.deleteRememberCheckbox.isChecked)
     }
 }
