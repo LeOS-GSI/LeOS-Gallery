da1269898f6ef719a084af575024e5a09e657e00
hojat <hojat72elect@gmail.com>
2022-10-10 22:40:40 -0400

    Ticket_18 Removed gradle dependency to AndroidSVG (com.caverock.androidsvg)

--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/svg/SvgDecoder.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/svg/SvgDecoder.kt
@@ -1,11 +1,11 @@
 package ca.on.sudbury.hojat.smartgallery.svg
 
+import ca.on.sudbury.hojat.smartgallery.svg.androidsvg.SVG
+import ca.on.sudbury.hojat.smartgallery.svg.androidsvg.SVGParseException
 import com.bumptech.glide.load.Options
 import com.bumptech.glide.load.ResourceDecoder
 import com.bumptech.glide.load.engine.Resource
 import com.bumptech.glide.load.resource.SimpleResource
-import com.caverock.androidsvg.SVG
-import com.caverock.androidsvg.SVGParseException
 
 import java.io.IOException
 import java.io.InputStream
@@ -15,7 +15,12 @@ class SvgDecoder : ResourceDecoder<InputStream, SVG> {
     override fun handles(source: InputStream, options: Options) = true
 
     @Throws(IOException::class)
-    override fun decode(source: InputStream, width: Int, height: Int, options: Options): Resource<SVG> {
+    override fun decode(
+        source: InputStream,
+        width: Int,
+        height: Int,
+        options: Options
+    ): Resource<SVG> {
         try {
             val svg = SVG.getFromInputStream(source)
             return SimpleResource(svg)
--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/svg/SvgDrawableTranscoder.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/svg/SvgDrawableTranscoder.kt
@@ -1,14 +1,17 @@
 package ca.on.sudbury.hojat.smartgallery.svg
 
 import android.graphics.drawable.PictureDrawable
+import ca.on.sudbury.hojat.smartgallery.svg.androidsvg.SVG
 import com.bumptech.glide.load.Options
 import com.bumptech.glide.load.engine.Resource
 import com.bumptech.glide.load.resource.SimpleResource
 import com.bumptech.glide.load.resource.transcode.ResourceTranscoder
-import com.caverock.androidsvg.SVG
 
 class SvgDrawableTranscoder : ResourceTranscoder<SVG, PictureDrawable> {
-    override fun transcode(toTranscode: Resource<SVG>, options: Options): Resource<PictureDrawable> {
+    override fun transcode(
+        toTranscode: Resource<SVG>,
+        options: Options
+    ): Resource<PictureDrawable> {
         val svg = toTranscode.get()
         val picture = svg.renderToPicture()
         val drawable = PictureDrawable(picture)
