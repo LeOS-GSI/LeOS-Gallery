857caeec292135587eb526876fb499ed9364df4d
hojat <hojat72elect@gmail.com>
2022-10-15 01:05:27 -0400

    Ticket_5 Migrated FAQActivity to ViewBinding

--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/activities/FAQActivity.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/activities/FAQActivity.kt
@@ -14,22 +14,29 @@ import ca.on.sudbury.hojat.smartgallery.extensions.getProperPrimaryColor
 import ca.on.sudbury.hojat.smartgallery.extensions.getProperTextColor
 import ca.on.sudbury.hojat.smartgallery.extensions.removeUnderlines
 import ca.on.sudbury.hojat.smartgallery.R
+import ca.on.sudbury.hojat.smartgallery.databinding.ActivityFaqBinding
+import ca.on.sudbury.hojat.smartgallery.databinding.ItemFaqBinding
 import ca.on.sudbury.hojat.smartgallery.helpers.APP_FAQ
 import ca.on.sudbury.hojat.smartgallery.helpers.APP_ICON_IDS
 import ca.on.sudbury.hojat.smartgallery.helpers.APP_LAUNCHER_NAME
 import ca.on.sudbury.hojat.smartgallery.models.FaqItem
-import kotlinx.android.synthetic.main.activity_faq.*
-import kotlinx.android.synthetic.main.item_faq.view.*
+import timber.log.Timber
 
 class FAQActivity : BaseSimpleActivity() {
-    override fun getAppIconIDs() = intent.getIntegerArrayListExtra(APP_ICON_IDS) ?: ArrayList()
 
+    private lateinit var activityBinding: ActivityFaqBinding
+
+    override fun getAppIconIDs() = intent.getIntegerArrayListExtra(APP_ICON_IDS) ?: ArrayList()
     override fun getAppLauncherName() = intent.getStringExtra(APP_LAUNCHER_NAME) ?: ""
 
     @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_faq)
+        activityBinding = ActivityFaqBinding.inflate(layoutInflater)
+
+        setContentView(activityBinding.root)
+
+        Timber.d("Hojat Ghasemi : The FAQActivity is called")
 
         val dividerMargin = resources.getDimension(R.dimen.medium_margin).toInt()
         val titleColor = getProperPrimaryColor()
@@ -38,17 +45,18 @@ class FAQActivity : BaseSimpleActivity() {
 
         val inflater = LayoutInflater.from(this)
         val faqItems = intent.getSerializableExtra(APP_FAQ) as ArrayList<FaqItem>
-        faqItems.forEach {
-            val faqItem = it
-            inflater.inflate(R.layout.item_faq, null).apply {
-                background.applyColorFilter(backgroundColor.getContrastColor())
-                faq_title.apply {
+
+        faqItems.forEach { faqItem ->
+
+            ItemFaqBinding.inflate(inflater).apply {
+                root.background.applyColorFilter(backgroundColor.getContrastColor())
+                this.faqTitle.apply {
                     text =
                         if (faqItem.title is Int) getString(faqItem.title) else faqItem.title as String
                     setTextColor(titleColor)
                 }
 
-                faq_text.apply {
+                faqText.apply {
                     text =
                         if (faqItem.text is Int) Html.fromHtml(getString(faqItem.text)) else faqItem.text as String
                     setTextColor(textColor)
@@ -57,8 +65,8 @@ class FAQActivity : BaseSimpleActivity() {
                     movementMethod = LinkMovementMethod.getInstance()
                     removeUnderlines()
                 }
-                faq_holder.addView(this)
-                (layoutParams as LinearLayout.LayoutParams).bottomMargin = dividerMargin
+                activityBinding.faqHolder.addView(root)
+                (root.layoutParams as LinearLayout.LayoutParams).bottomMargin = dividerMargin
             }
         }
     }
