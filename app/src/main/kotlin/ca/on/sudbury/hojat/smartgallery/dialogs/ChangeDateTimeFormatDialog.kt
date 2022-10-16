70890b3648d688de4cac641f65e5cde819c5bf56
hojat <hojat72elect@gmail.com>
2022-10-15 20:10:51 -0400

    Ticket_5 Migrated ChangeDateTimeFormatDialog to ViewBinding

--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/ChangeDateTimeFormatDialog.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/ChangeDateTimeFormatDialog.kt
@@ -1,7 +1,5 @@
 package ca.on.sudbury.hojat.smartgallery.dialogs
 
-
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.text.format.DateFormat
 import ca.on.sudbury.hojat.smartgallery.helpers.DATE_FORMAT_EIGHT
@@ -20,56 +18,58 @@ import ca.on.sudbury.hojat.smartgallery.R.id.change_date_time_dialog_radio_four
 import ca.on.sudbury.hojat.smartgallery.R.id.change_date_time_dialog_radio_five
 import ca.on.sudbury.hojat.smartgallery.R.id.change_date_time_dialog_radio_six
 import ca.on.sudbury.hojat.smartgallery.R.id.change_date_time_dialog_radio_seven
+import ca.on.sudbury.hojat.smartgallery.databinding.DialogChangeDateTimeFormatBinding
 import ca.on.sudbury.hojat.smartgallery.extensions.baseConfig
 import ca.on.sudbury.hojat.smartgallery.extensions.getAlertDialogBuilder
 import ca.on.sudbury.hojat.smartgallery.extensions.setupDialogStuff
-import kotlinx.android.synthetic.main.dialog_change_date_time_format.view.*
 import java.util.Calendar
 import java.util.Locale
 
-class ChangeDateTimeFormatDialog(val activity: Activity, val callback: () -> Unit) {
-    @SuppressLint("InflateParams")
-    private val view =
-        activity.layoutInflater.inflate(R.layout.dialog_change_date_time_format, null)!!
+/**
+ * In the Settings page, click on "Change date and time format". The dialog is created by this class.
+ */
+class ChangeDateTimeFormatDialog(
+    val activity: Activity,
+    val callback: () -> Unit
+) {
+    private val binding = DialogChangeDateTimeFormatBinding.inflate(activity.layoutInflater)
     private val sampleTS = 1613422500000    // February 15, 2021
 
     init {
-        view.apply {
-            change_date_time_dialog_radio_one.text = formatDateSample(DATE_FORMAT_ONE)
-            change_date_time_dialog_radio_two.text = formatDateSample(DATE_FORMAT_TWO)
-            change_date_time_dialog_radio_three.text = formatDateSample(DATE_FORMAT_THREE)
-            change_date_time_dialog_radio_four.text = formatDateSample(DATE_FORMAT_FOUR)
-            change_date_time_dialog_radio_five.text = formatDateSample(DATE_FORMAT_FIVE)
-            change_date_time_dialog_radio_six.text = formatDateSample(DATE_FORMAT_SIX)
-            change_date_time_dialog_radio_seven.text = formatDateSample(DATE_FORMAT_SEVEN)
-            change_date_time_dialog_radio_eight.text = formatDateSample(DATE_FORMAT_EIGHT)
-
-            change_date_time_dialog_24_hour.isChecked = activity.baseConfig.use24HourFormat
+        binding.apply {
+            changeDateTimeDialogRadioOne.text = formatDateSample(DATE_FORMAT_ONE)
+            changeDateTimeDialogRadioTwo.text = formatDateSample(DATE_FORMAT_TWO)
+            changeDateTimeDialogRadioThree.text = formatDateSample(DATE_FORMAT_THREE)
+            changeDateTimeDialogRadioFour.text = formatDateSample(DATE_FORMAT_FOUR)
+            changeDateTimeDialogRadioFive.text = formatDateSample(DATE_FORMAT_FIVE)
+            changeDateTimeDialogRadioSix.text = formatDateSample(DATE_FORMAT_SIX)
+            changeDateTimeDialogRadioSeven.text = formatDateSample(DATE_FORMAT_SEVEN)
+            changeDateTimeDialogRadioEight.text = formatDateSample(DATE_FORMAT_EIGHT)
+            changeDateTimeDialog24Hour.isChecked = activity.baseConfig.use24HourFormat
 
             val formatButton = when (activity.baseConfig.dateFormat) {
-                DATE_FORMAT_ONE -> change_date_time_dialog_radio_one
-                DATE_FORMAT_TWO -> change_date_time_dialog_radio_two
-                DATE_FORMAT_THREE -> change_date_time_dialog_radio_three
-                DATE_FORMAT_FOUR -> change_date_time_dialog_radio_four
-                DATE_FORMAT_FIVE -> change_date_time_dialog_radio_five
-                DATE_FORMAT_SIX -> change_date_time_dialog_radio_six
-                DATE_FORMAT_SEVEN -> change_date_time_dialog_radio_seven
-                else -> change_date_time_dialog_radio_eight
+                DATE_FORMAT_ONE -> changeDateTimeDialogRadioOne
+                DATE_FORMAT_TWO -> changeDateTimeDialogRadioTwo
+                DATE_FORMAT_THREE -> changeDateTimeDialogRadioThree
+                DATE_FORMAT_FOUR -> changeDateTimeDialogRadioFour
+                DATE_FORMAT_FIVE -> changeDateTimeDialogRadioFive
+                DATE_FORMAT_SIX -> changeDateTimeDialogRadioSix
+                DATE_FORMAT_SEVEN -> changeDateTimeDialogRadioSeven
+                else -> changeDateTimeDialogRadioEight
             }
             formatButton.isChecked = true
         }
-
         activity.getAlertDialogBuilder()
             .setPositiveButton(R.string.ok) { _, _ -> dialogConfirmed() }
             .setNegativeButton(R.string.cancel, null)
             .apply {
-                activity.setupDialogStuff(view, this)
+                activity.setupDialogStuff(binding.root, this)
             }
     }
 
     private fun dialogConfirmed() {
         activity.baseConfig.dateFormat =
-            when (view.change_date_time_dialog_radio_group.checkedRadioButtonId) {
+            when (binding.changeDateTimeDialogRadioGroup.checkedRadioButtonId) {
                 change_date_time_dialog_radio_one -> DATE_FORMAT_ONE
                 change_date_time_dialog_radio_two -> DATE_FORMAT_TWO
                 change_date_time_dialog_radio_three -> DATE_FORMAT_THREE
@@ -80,7 +80,7 @@ class ChangeDateTimeFormatDialog(val activity: Activity, val callback: () -> Uni
                 else -> DATE_FORMAT_EIGHT
             }
 
-        activity.baseConfig.use24HourFormat = view.change_date_time_dialog_24_hour.isChecked
+        activity.baseConfig.use24HourFormat = binding.changeDateTimeDialog24Hour.isChecked
         callback()
     }
 
