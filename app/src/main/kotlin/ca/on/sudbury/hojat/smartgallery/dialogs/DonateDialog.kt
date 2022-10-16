42acc044328493aa31ba61f9013b0a7cdff8bf47
hojat <hojat72elect@gmail.com>
2022-10-16 13:22:50 -0400

    Ticket_5 Migrated DonateDialog to ViewBinding

--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/DonateDialog.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/DonateDialog.kt
@@ -1,28 +1,31 @@
 package ca.on.sudbury.hojat.smartgallery.dialogs
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.text.Html
 import android.text.method.LinkMovementMethod
 import ca.on.sudbury.hojat.smartgallery.R
+import ca.on.sudbury.hojat.smartgallery.databinding.DialogTextviewBinding
 import ca.on.sudbury.hojat.smartgallery.extensions.getAlertDialogBuilder
 import ca.on.sudbury.hojat.smartgallery.extensions.launchViewIntent
 import ca.on.sudbury.hojat.smartgallery.extensions.setupDialogStuff
-import kotlinx.android.synthetic.main.dialog_textview.view.*
+import timber.log.Timber
 
-@SuppressLint("InflateParams")
+/**
+ * where it's called ?
+ */
 class DonateDialog(val activity: Activity) {
     init {
-        val view = activity.layoutInflater.inflate(R.layout.dialog_textview, null).apply {
-            text_view.text = Html.fromHtml(activity.getString(R.string.donate_please))
-            text_view.movementMethod = LinkMovementMethod.getInstance()
+        Timber.d("Hojat Ghasemi : DonateDialog was called")
+        val binding = DialogTextviewBinding.inflate(activity.layoutInflater).apply {
+            textView.text = Html.fromHtml(activity.getString(R.string.donate_please))
+            textView.movementMethod = LinkMovementMethod.getInstance()
         }
 
         activity.getAlertDialogBuilder()
             .setPositiveButton(R.string.purchase) { _, _ -> activity.launchViewIntent(R.string.thank_you_url) }
             .setNegativeButton(R.string.cancel, null)
             .apply {
-                activity.setupDialogStuff(view, this, cancelOnTouchOutside = false)
+                activity.setupDialogStuff(binding.root, this, cancelOnTouchOutside = false)
             }
     }
 }
