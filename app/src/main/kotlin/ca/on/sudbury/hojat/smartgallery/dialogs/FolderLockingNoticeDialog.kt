5c34b7579e0348c49be8cbed4950436e49273ac0
hojat <hojat72elect@gmail.com>
2022-10-16 18:48:39 -0400

    Ticket_5 Migrated FolderLockingNoticeDialog to ViewBinding

--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/FolderLockingNoticeDialog.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/FolderLockingNoticeDialog.kt
@@ -1,26 +1,27 @@
 package ca.on.sudbury.hojat.smartgallery.dialogs
 
-
 import android.annotation.SuppressLint
 import android.app.Activity
 import ca.on.sudbury.hojat.smartgallery.R
+import ca.on.sudbury.hojat.smartgallery.databinding.DialogTextviewBinding
 import ca.on.sudbury.hojat.smartgallery.extensions.baseConfig
 import ca.on.sudbury.hojat.smartgallery.extensions.getAlertDialogBuilder
 import ca.on.sudbury.hojat.smartgallery.extensions.setupDialogStuff
-import kotlinx.android.synthetic.main.dialog_textview.view.*
+import timber.log.Timber
 
 @SuppressLint("InflateParams")
 class FolderLockingNoticeDialog(val activity: Activity, val callback: () -> Unit) {
     init {
-        val view = activity.layoutInflater.inflate(R.layout.dialog_textview, null).apply {
-            text_view.text = activity.getString(R.string.lock_folder_notice)
+        Timber.d("Hojat Ghasemi : FolderLockingNoticeDialog was called")
+        val binding = DialogTextviewBinding.inflate(activity.layoutInflater).apply {
+            textView.text = activity.getString(R.string.lock_folder_notice)
         }
 
         activity.getAlertDialogBuilder()
             .setPositiveButton(R.string.ok) { _, _ -> dialogConfirmed() }
             .setNegativeButton(R.string.cancel, null)
             .apply {
-                activity.setupDialogStuff(view, this, R.string.disclaimer)
+                activity.setupDialogStuff(binding.root, this, R.string.disclaimer)
             }
     }
 
