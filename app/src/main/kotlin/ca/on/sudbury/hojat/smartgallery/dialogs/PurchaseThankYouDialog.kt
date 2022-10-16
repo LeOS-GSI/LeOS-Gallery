ff48e4c70d3ccce042dd2283e0996afab6a3647a
hojat <hojat72elect@gmail.com>
2022-10-16 13:03:40 -0400

    Ticket_5 Migrated PurchaseThankYouDialog to ViewBinding

--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/PurchaseThankYouDialog.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/PurchaseThankYouDialog.kt
@@ -1,36 +1,37 @@
 package ca.on.sudbury.hojat.smartgallery.dialogs
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.text.Html
 import android.text.method.LinkMovementMethod
 import androidx.appcompat.app.AlertDialog
 import ca.on.sudbury.hojat.smartgallery.R
+import ca.on.sudbury.hojat.smartgallery.databinding.DialogPurchaseThankYouBinding
 import ca.on.sudbury.hojat.smartgallery.extensions.baseConfig
 import ca.on.sudbury.hojat.smartgallery.extensions.launchPurchaseThankYouIntent
 import ca.on.sudbury.hojat.smartgallery.extensions.removeUnderlines
 import ca.on.sudbury.hojat.smartgallery.extensions.setupDialogStuff
-import kotlinx.android.synthetic.main.dialog_purchase_thank_you.view.*
 
-@SuppressLint("InflateParams")
+/**
+ * Where this dialog is called ?
+ */
 class PurchaseThankYouDialog(val activity: Activity) {
     init {
-        val view = activity.layoutInflater.inflate(R.layout.dialog_purchase_thank_you, null).apply {
+        val binding = DialogPurchaseThankYouBinding.inflate(activity.layoutInflater).apply {
             var text = activity.getString(R.string.purchase_thank_you)
             if (activity.baseConfig.appId.removeSuffix(".debug").endsWith(".pro")) {
                 text += "<br><br>${activity.getString(R.string.shared_theme_note)}"
             }
 
-            purchase_thank_you.text = Html.fromHtml(text)
-            purchase_thank_you.movementMethod = LinkMovementMethod.getInstance()
-            purchase_thank_you.removeUnderlines()
+            purchaseThankYou.text = Html.fromHtml(text)
+            purchaseThankYou.movementMethod = LinkMovementMethod.getInstance()
+            purchaseThankYou.removeUnderlines()
         }
 
         AlertDialog.Builder(activity)
             .setPositiveButton(R.string.purchase) { _, _ -> activity.launchPurchaseThankYouIntent() }
             .setNegativeButton(R.string.cancel, null)
             .create().apply {
-                activity.setupDialogStuff(view, this, cancelOnTouchOutside = false)
+                activity.setupDialogStuff(binding.root, this, cancelOnTouchOutside = false)
             }
     }
 }
