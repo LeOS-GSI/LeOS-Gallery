b0140233b182a0d5dfb078ca6fc59337b3529532
hojat <hojat72elect@gmail.com>
2022-10-17 00:56:24 -0400

    Ticket_5 Migrated RenameDialog to ViewBinding

--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/RenameDialog.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/RenameDialog.kt
@@ -1,8 +1,5 @@
 package ca.on.sudbury.hojat.smartgallery.dialogs
 
-
-import android.annotation.SuppressLint
-import android.view.LayoutInflater
 import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
 import ca.on.sudbury.hojat.smartgallery.extensions.baseConfig
@@ -14,12 +11,13 @@ import ca.on.sudbury.hojat.smartgallery.extensions.setupDialogStuff
 import ca.on.sudbury.hojat.smartgallery.R
 import ca.on.sudbury.hojat.smartgallery.activities.BaseSimpleActivity
 import ca.on.sudbury.hojat.smartgallery.adapters.RenameAdapter
+import ca.on.sudbury.hojat.smartgallery.databinding.DialogRenameBinding
 import ca.on.sudbury.hojat.smartgallery.extensions.onPageChangeListener
 import ca.on.sudbury.hojat.smartgallery.extensions.onTabSelectionChanged
 import ca.on.sudbury.hojat.smartgallery.helpers.RENAME_PATTERN
 import ca.on.sudbury.hojat.smartgallery.helpers.RENAME_SIMPLE
 import ca.on.sudbury.hojat.smartgallery.views.MyViewPager
-import kotlinx.android.synthetic.main.dialog_rename.view.*
+import timber.log.Timber
 
 class RenameDialog(
     val activity: BaseSimpleActivity,
@@ -27,41 +25,41 @@ class RenameDialog(
     private val useMediaFileExtension: Boolean,
     val callback: () -> Unit
 ) {
-    var dialog: AlertDialog? = null
 
-    @SuppressLint("InflateParams")
-    val view = LayoutInflater.from(activity).inflate(R.layout.dialog_rename, null)!!
+    var dialog: AlertDialog? = null
+    val binding = DialogRenameBinding.inflate(activity.layoutInflater)
     private var tabsAdapter: RenameAdapter
     var viewPager: MyViewPager
 
     init {
-        view.apply {
-            viewPager = findViewById(R.id.dialog_tab_view_pager)
+        Timber.d("Hojat Ghasemi : RenameDialog was called")
+        binding.apply {
+            viewPager = dialogTabViewPager
             tabsAdapter = RenameAdapter(activity, paths)
             viewPager.adapter = tabsAdapter
             viewPager.onPageChangeListener {
-                dialog_tab_layout.getTabAt(it)!!.select()
+                dialogTabLayout.getTabAt(it)!!.select()
             }
             viewPager.currentItem = activity.baseConfig.lastRenameUsed
 
             if (activity.baseConfig.isUsingSystemTheme) {
-                dialog_tab_layout.setBackgroundColor(activity.resources.getColor(R.color.you_dialog_background_color))
+                dialogTabLayout.setBackgroundColor(activity.resources.getColor(R.color.you_dialog_background_color))
             } else {
-                dialog_tab_layout.setBackgroundColor(context.getProperBackgroundColor())
+                dialogTabLayout.setBackgroundColor(root.context.getProperBackgroundColor())
             }
 
-            val textColor = context.getProperTextColor()
-            dialog_tab_layout.setTabTextColors(textColor, textColor)
-            dialog_tab_layout.setSelectedTabIndicatorColor(context.getProperPrimaryColor())
+            val textColor = root.context.getProperTextColor()
+            dialogTabLayout.setTabTextColors(textColor, textColor)
+            dialogTabLayout.setSelectedTabIndicatorColor(root.context.getProperPrimaryColor())
 
             if (activity.baseConfig.isUsingSystemTheme) {
-                dialog_tab_layout.setBackgroundColor(activity.resources.getColor(R.color.you_dialog_background_color))
+                dialogTabLayout.setBackgroundColor(activity.resources.getColor(R.color.you_dialog_background_color))
             }
 
-            dialog_tab_layout.onTabSelectionChanged(tabSelectedAction = {
+            dialogTabLayout.onTabSelectionChanged(tabSelectedAction = {
                 viewPager.currentItem = when {
                     it.text.toString().equals(
-                        resources.getString(R.string.simple_renaming),
+                        root.resources.getString(R.string.simple_renaming),
                         true
                     ) -> RENAME_SIMPLE
                     else -> RENAME_PATTERN
@@ -73,7 +71,7 @@ class RenameDialog(
             .setPositiveButton(R.string.ok, null)
             .setNegativeButton(R.string.cancel) { _, _ -> dismissDialog() }
             .apply {
-                activity.setupDialogStuff(view, this) { alertDialog ->
+                activity.setupDialogStuff(binding.root, this) { alertDialog ->
                     dialog = alertDialog
                     alertDialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM)
                     alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
