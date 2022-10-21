c90c0b99dcaf14ab1527f6ba906e785727267fa7
hojat <hojat72elect@gmail.com>
2022-10-21 18:11:02 -0400

    Ticket_27  Created DetermineApiLevelUseCases

---
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/photoedit/usecases/DetermineApiLevelUseCases.kt
@@ -0,0 +1,31 @@
+package ca.on.sudbury.hojat.smartgallery.photoedit.usecases
+
+import android.os.Build
+
+object IsMarshmallowPlusUseCase {
+    operator fun invoke() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+}
+
+object IsNougatPlusUseCase {
+    operator fun invoke() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+}
+
+object IsOreoPlusUseCase {
+    operator fun invoke() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+}
+
+object IsPiePlusUseCase {
+    operator fun invoke() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
+}
+
+object IsQPlusUseCase {
+    operator fun invoke() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+}
+
+object IsRPlusUseCase {
+    operator fun invoke() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+}
+
+object IsSPlusUseCase {
+    operator fun invoke() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+}
