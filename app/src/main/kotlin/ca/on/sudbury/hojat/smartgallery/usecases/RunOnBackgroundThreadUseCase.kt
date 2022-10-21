a64f2132be8d128716035c29e4b7920a0918a71b
hojat <hojat72elect@gmail.com>
2022-10-21 17:00:35 -0400

    Ticket_27  Created RunOnBackgroundThreadUseCase

--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/usecases/RunOnBackgroundThreadUseCase.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/usecases/RunOnBackgroundThreadUseCase.kt
@@ -1,5 +1,8 @@
 package ca.on.sudbury.hojat.smartgallery.usecases
 
+/**
+ * You just give it a block of code and it makes sure the callback will run on background thread. Later on, I will migrate it to Coroutines.
+ */
 object RunOnBackgroundThreadUseCase {
     operator fun invoke(callback: () -> Unit) {
         if (IsMainThreadUseCase()) {
