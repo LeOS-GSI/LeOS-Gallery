19f5cd456532d79c208545f499bcee0e1b9a937b
hojat <hojat72elect@gmail.com>
2022-10-23 23:54:43 -0400

    Ticket_27 Removed "App" extensions

--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/App.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/App.kt
@@ -2,23 +2,35 @@ package ca.on.sudbury.hojat.smartgallery
 
 import android.app.Application
 import ca.on.hojat.fingerprint.core.Reprint
-import ca.on.sudbury.hojat.smartgallery.extensions.checkUseEnglish
+import ca.on.sudbury.hojat.smartgallery.extensions.baseConfig
+import ca.on.sudbury.hojat.smartgallery.photoedit.usecases.IsNougatPlusUseCase
 import com.squareup.picasso.Downloader
 import com.squareup.picasso.Picasso
 import okhttp3.Request
 import okhttp3.Response
 import timber.log.Timber
+import java.util.Locale
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        checkUseEnglish()
+
+        // Check if the device is using English
+        if (baseConfig.useEnglish && !IsNougatPlusUseCase()) {
+            val conf = resources.configuration
+            conf.locale = Locale.ENGLISH
+            resources.updateConfiguration(conf, resources.displayMetrics)
+        }
+
+
         Reprint.initialize(this)
         Picasso.setSingletonInstance(Picasso.Builder(this).downloader(object : Downloader {
             override fun load(request: Request) = Response.Builder().build()
 
             override fun shutdown() {}
         }).build())
+
+        // Plant the Timber
         Timber.plant(Timber.DebugTree())
     }
 }
