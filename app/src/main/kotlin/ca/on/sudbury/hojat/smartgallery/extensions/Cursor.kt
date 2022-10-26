5dffc2fc747b6e40528e407fef3608a2a08b75aa
hojat <hojat72elect@gmail.com>
2022-10-26 15:54:02 -0400

    Ticket_27 :

--- app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/extensions/Cursor.kt
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/extensions/Cursor.kt
@@ -3,6 +3,9 @@ package ca.on.sudbury.hojat.smartgallery.extensions
 import android.annotation.SuppressLint
 import android.database.Cursor
 
+/**
+ * I have decided to put up with these extension functions for now. They're not too painful.
+ */
 @SuppressLint("Range")
 fun Cursor.getStringValue(key: String): String = getString(getColumnIndex(key))
 
