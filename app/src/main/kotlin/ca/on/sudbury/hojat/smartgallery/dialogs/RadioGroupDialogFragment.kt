ea2fe7fc1871b6bb79f282e4f99c77bf16520102
hojat <hojat72elect@gmail.com>
2022-11-24 15:42:08 -0500

    Ticket_29 : Created RadioGroupDialogFragment.kt

---
+++ app/src/main/kotlin/ca/on/sudbury/hojat/smartgallery/dialogs/RadioGroupDialogFragment.kt
@@ -0,0 +1,117 @@
+package ca.on.sudbury.hojat.smartgallery.dialogs
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.fragment.app.DialogFragment
+import ca.on.sudbury.hojat.smartgallery.R
+import ca.on.sudbury.hojat.smartgallery.databinding.DialogFragmentRadioGroupBinding
+import ca.on.sudbury.hojat.smartgallery.extensions.onGlobalLayout
+import ca.on.sudbury.hojat.smartgallery.models.RadioItem
+
+/**
+ * There are various dialogs that use this class. I need to firstly force them to use
+ * a pre-created XML UI and then delete this class.
+ */
+class RadioGroupDialogFragment(
+    val items: ArrayList<RadioItem>,
+    private val checkedItemId: Int = -1,
+    private val titleId: Int = 0,
+    showOKButton: Boolean = false,
+    private val cancelCallback: (() -> Unit)? = null,
+    val callback: (newValue: Any) -> Unit
+) : DialogFragment() {
+
+    // the binding
+    private var _binding: DialogFragmentRadioGroupBinding? = null
+    private val binding get() = _binding!!
+
+    // some configurations
+    private var wasInit = false
+    private var selectedItemId = -1
+
+
+    /**
+     * Create the UI of the dialog
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogFragmentRadioGroupBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        loadDialogUI()
+        return binding.root
+    }
+
+    @SuppressLint("InflateParams")
+    private fun loadDialogUI() {
+        binding.dialogRadioGroup.apply {
+            for (i in 0 until items.size) {
+                val radioButton = (layoutInflater.inflate(
+                    R.layout.radio_button,
+                    null
+                ) as RadioButton).apply {
+                    text = items[i].title
+                    isChecked = items[i].id == checkedItemId
+                    id = i
+                    setOnClickListener { itemSelected(i) }
+                }
+
+                if (items[i].id == checkedItemId) {
+                    selectedItemId = i
+                }
+
+                addView(
+                    radioButton,
+                    RadioGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                    )
+                )
+            }
+        }
+        if (selectedItemId != -1) {
+            binding.dialogRadioHolder.apply {
+                onGlobalLayout {
+                    scrollY =
+                        binding.dialogRadioGroup.findViewById<View>(selectedItemId).bottom - height
+                }
+            }
+        }
+
+        wasInit = true
+    }
+
+    /**
+     * Register listeners for views.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dialog?.setOnCancelListener { cancelCallback?.invoke() }
+    }
+
+    /**
+     * Cleaning the stuff
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun itemSelected(checkedId: Int) {
+        if (wasInit) {
+            callback(items[checkedId].value)
+            dismiss()
+        }
+    }
+}
