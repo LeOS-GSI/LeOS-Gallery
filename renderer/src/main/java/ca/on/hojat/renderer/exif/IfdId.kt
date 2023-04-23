1b8ee5484f7809714df3a56990a4c32c36790100
hojat <hojat72elect@gmail.com>
2023-04-23 02:28:06 -0400

    IfdId.java to kt

--- renderer/src/main/java/ca/on/hojat/renderer/exif/IfdId.kt
+++ renderer/src/main/java/ca/on/hojat/renderer/exif/IfdId.kt
@@ -1,15 +1,17 @@
-package ca.on.hojat.renderer.exif;
+package ca.on.hojat.renderer.exif
 
 /**
  * The constants of the IFD ID defined in EXIF spec.
  */
-public interface IfdId {
-    int TYPE_IFD_0 = 0;
-    int TYPE_IFD_1 = 1;
-    int TYPE_IFD_EXIF = 2;
-    int TYPE_IFD_INTEROPERABILITY = 3;
-    int TYPE_IFD_GPS = 4;
-    // This is used in ExifData to allocate enough IfdData
-    int TYPE_IFD_COUNT = 5;
+interface IfdId {
+    companion object {
+        const val TYPE_IFD_0 = 0
+        const val TYPE_IFD_1 = 1
+        const val TYPE_IFD_EXIF = 2
+        const val TYPE_IFD_INTEROPERABILITY = 3
+        const val TYPE_IFD_GPS = 4
 
+        // This is used in ExifData to allocate enough IfdData
+        const val TYPE_IFD_COUNT = 5
+    }
 }
